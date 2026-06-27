42fb94431e7e935f0dff838d237d2d0d2c91ae8c
Kidus-Sofonias <sofoniaskidus@gmail.com>
2026-06-27 05:11:20 +0300

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,13 @@
+export const navLinks = [
+  { label: "Store" },
+  { label: "Mac" },
+  { label: "iPad" },
+  { label: "iPhone" },
+  { label: "Watch" },
+  { label: "Vision" },
+  { label: "AirPods" },
+  { label: "TV & Home" },
+  { label: "Entertainment" },
+  { label: "Accessories" },
+  { label: "Support" },
+];
